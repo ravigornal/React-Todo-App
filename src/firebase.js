875d78cf044f05bf6138846a2b1505ae858bb357
Ravi <ravigornal@caliperbusiness.com>
2020-11-03 10:42:41 +0530

    todo basic

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,14 @@
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyC4jhjO3TSAI7Rwux2H1MFBZXxqWpbLHn0",
-    authDomain: "todo-app-21f07.firebaseapp.com",
-    databaseURL: "https://todo-app-21f07.firebaseio.com",
-    projectId: "todo-app-21f07",
-    storageBucket: "todo-app-21f07.appspot.com",
-    messagingSenderId: "1060702997001",
-    appId: "1:1060702997001:web:9fa5ae6d0389c7a9f406d8",
-    measurementId: "G-PM0NLJL9QH"
-
+    apiKey: "AIzaSyAINbYLrivy-cRETEDk7fG-EYHBTMbLaBw",
+    authDomain: "react-todo-84710.firebaseapp.com",
+    databaseURL: "https://react-todo-84710.firebaseio.com",
+    projectId: "react-todo-84710",
+    storageBucket: "react-todo-84710.appspot.com",
+    messagingSenderId: "855824819551",
+    appId: "1:855824819551:web:75f3d0a2a3c5c50f8902ea",
+    measurementId: "G-ZRKYMZ3WZ3"
 });
 
 
